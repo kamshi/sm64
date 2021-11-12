63e50a044d05e6cdd64fec22bb2b4312523e1368
Sebastian Jähne <jaehnesn@gmx.de>
2021-11-12 19:06:24 +0100

    filled compile_commands.json with values

--- create_compile_database.sh
+++ create_compile_database.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # you need to install bear in order to create the compile database
+# this will override the existing compiler_commands.json
+# you have to run make clean before executing bear, otherwise it will be incomplete
+make clean
 bear -- make VERSION=eu
