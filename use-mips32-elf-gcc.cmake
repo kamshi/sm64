4453861d3aa692f9e6651b1069aba5ebb9de11e2
Sebastian Jähne <jaehnesn@gmx.de>
2024-10-13 15:16:34 +0200

    disabled compiler checks in toolchain for mips32

--- use-mips32-elf-gcc.cmake
+++ use-mips32-elf-gcc.cmake
@@ -4,13 +4,16 @@ set(CMAKE_SYSTEM_PROCESSOR mips)
 
 # specify the compiler paths
 set(CMAKE_C_COMPILER mips32-elf-gcc)
-#set(CMAKE_CXX_COMPILER mips32-elf-g++)
+set(CMAKE_CXX_COMPILER mips32-elf-g++)
 set(CMAKE_LINKER mips32-elf-ld)
 set(CMAKE_ASM_COMPILER mips32-elf-as)
 set(CMAKE_AR mips32-elf-ar)
 set(CMAKE_RANLIB mips32-elf-ranlib)
 set(CMAKE_OBJCOPY mips32-elf-objcopy)
 set(CMAKE_NM mips32-elf-nm)
+# skip checks whether compilers are working
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
 
 # optionally, specify the sysroot if available
 #set(CMAKE_SYSROOT /path/to/sysroot)
